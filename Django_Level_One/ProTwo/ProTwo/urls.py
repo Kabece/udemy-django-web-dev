cc5361a42fb8907bcd9d8ac5ee8aadd8c1440b79
Kabece <kabece@gmail.com>
2017-07-31 20:36:03 +0200

    Python and Django Full Stack Web Developer Bootcamp Finished

--- Django_Level_One/ProTwo/ProTwo/urls.py
+++ Django_Level_One/ProTwo/ProTwo/urls.py
@@ -17,9 +17,16 @@ from django.conf.urls import url
 from django.contrib import admin
 from AppTwo import views
 from django.conf.urls import include
+from django.conf import settings
 
 urlpatterns = [
     url(r'^$', views.index, name = "index"),
     url(r'^users/', include('AppTwo.urls')),
     url(r'^admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    import  debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
